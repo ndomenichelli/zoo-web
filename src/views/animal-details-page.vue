04ec98423495bad29b0fd0b4fa854d74e004f592
ndomenichelli <61430097+ndomenichelli@users.noreply.github.com>
2022-07-20 08:36:23 -0700

    details page center image

--- src/views/animal-details-page.vue
+++ src/views/animal-details-page.vue
@@ -1,10 +1,12 @@
 <template>
-  <div v-if="animal">
-    <h1>{{ animal.name }}</h1>
-    <img v-if="animal.image != ''" class="image" :src="animal.image" />
-    <p>Type of animal: {{ animal.type }}</p>
-    <p>This animals was born on {{ animal.birthdate }}</p>
-    <p>{{ animal.description }}</p>
+  <div class="animal-page">
+    <div v-if="animal">
+      <h1>{{ animal.name }}</h1>
+      <img v-if="animal.image != ''" class="image" :src="animal.image" />
+      <p>Type of animal: {{ animal.type }}</p>
+      <p>This animals was born on: {{ animal.birthdate }}</p>
+      <p>{{ animal.description }}</p>
+    </div>
   </div>
 </template>
 
@@ -27,21 +29,21 @@ export default {
       console.log(snapshot.val())
 
       this.animal = snapshot.val()
-
-      // Object.entries(data).forEach(([animalKey, animalElement]) => {
-      //   console.log(animalKey)
-      //   this.animals.push(animalElement)
-      // })
-
-      // console.log(this.animal)
     })
   }
 }
 </script>
 <style lang="scss">
-.image {
-  border: 1px solid black;
-  width: 100%;
-  height: 100%;
+.animal-page {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 40%;
+
+  .image {
+    border: 1px solid black;
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
