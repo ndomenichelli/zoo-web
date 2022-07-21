16e2f6f34797645d36f832854de0385af8179da9
ndomenichelli <61430097+ndomenichelli@users.noreply.github.com>
2022-07-21 08:13:09 -0700

    production build

--- src/views/admin-page.vue
+++ src/views/admin-page.vue
@@ -103,7 +103,7 @@ export default {
         dateAdded: Date.now()
       })
 
-      console.log('data ' + newAnimalsRef.key)
+      // console.log('data ' + newAnimalsRef.key)
       this.id = newAnimalsRef.key
     },
 
@@ -112,7 +112,7 @@ export default {
     },
 
     previewImage (event) {
-      console.log('previewImage')
+      // console.log('previewImage')
       this.uploadValue = 0
       this.img1 = null
       this.onUpload()
@@ -120,13 +120,13 @@ export default {
     // drop picture into dropbox
     drop (event) {
       this.imageData = event.dataTransfer.files[0]
-      console.log(this.imageData)
+      // console.log(this.imageData)
       this.previewImage()
     },
     // manually select picture
     selectedFile () {
       this.imageData = document.querySelector('.dropzoneFile').files[0]
-      console.log(this.imageData)
+      // console.log(this.imageData)
       this.previewImage()
     },
 
@@ -140,36 +140,36 @@ export default {
       // 1. 'state_changed' observer, called any time the state changes
       // 2. Error observer, called on failure
       // 3. Completion observer, called on successful completion
-      console.log('upload task check')
+      // console.log('upload task check')
       uploadTask.on(
         'state_changed',
         (snapshot) => {
           // Observe state change events such as progress, pause, and resume
           // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-          console.log('Upload is ' + progress + '% done')
+          // const progress =
+          //   (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          // console.log('Upload is ' + progress + '% done')
           switch (snapshot.state) {
             case 'paused':
-              console.log('Upload is paused')
+              // console.log('Upload is paused')
               break
             case 'running':
-              console.log('Upload is running')
+              // console.log('Upload is running')
               break
           }
         },
-        (error) => {
-          // Handle unsuccessful uploads
-          console.log('Unsuccessful Upload: ' + error)
-        },
+        // (error) => {
+        //   // Handle unsuccessful uploads
+        //   // console.log('Unsuccessful Upload: ' + error)
+        // },
         () => {
           // Handle successful uploads on complete
           // For instance, get the download URL: https://firebasestorage.googleapis.com/...
           this.uploadValue = 100
           getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            console.log('File available at', downloadURL)
+            // console.log('File available at', downloadURL)
             this.img1 = downloadURL
-            console.log(this.img1)
+            // console.log(this.img1)
           })
         }
       )
@@ -187,7 +187,17 @@ export default {
     padding: 10px;
     column-gap: 16px;
     label {
-      align-items: left;
+      width: 30%;
+      text-align: right;
+    }
+    label + input {
+      width: 15%;
+      margin: 0 30% 0 2%;
+    }
+    label + textarea {
+      width: 15%;
+      margin: 0 30% 0 2%;
+      resize: vertical;
     }
   }
   .dropzone-container {
